a8266a656888252726f9aeb76e52bfa7272e1bf8
Alexey Plekhanov <oleksii.plekhanov@gmail.com>
2013-12-14 10:50:51 +0200

    minor changes

--- src/alexsoft/laravel-curl/Curl.php
+++ src/alexsoft/laravel-curl/Curl.php
@@ -2,4 +2,28 @@
 
 class Curl {
 	const VERSION = '0.0.1';
+
+	const GET = 'GET';
+	const POST = 'POST';
+	const PUT = 'PUT';
+	const DELETE = 'DELETE';
+	const HEAD = 'HEAD';
+	const CONNECT = 'CONNECT';
+	const OPTION = 'OPTION';
+	const PATCH = 'PATCH';
+
+	/**
+	 * User agent for requests
+	 * @var string
+	 */
+	protected $_userAgent;
+
+	public function __construct() {
+		$this->setUserAgent('alexsoft/laravel-curl');
+	}
+
+	public function setUserAgent($userAgent) {
+		$this->_userAgent = $userAgent;
+		return $this;
+	}
 }
