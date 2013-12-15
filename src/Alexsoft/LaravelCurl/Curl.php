c47da767dbb12bafc98a25db9bcaaa24093b761c
Alexey Plekhanov <oleksii.plekhanov@gmail.com>
2013-12-15 23:27:26 +0200

    improvements

--- src/Alexsoft/LaravelCurl/Curl.php
+++ src/Alexsoft/LaravelCurl/Curl.php
@@ -13,6 +13,7 @@ class Curl {
 	const OPTION = 'OPTION';
 	const PATCH = 'PATCH';
 
+	protected $_request;
 	/**
 	 * User agent for requests
 	 * @var string
@@ -26,5 +27,57 @@ class Curl {
 	public function setUserAgent($userAgent) {
 		$this->_userAgent = $userAgent;
 		return $this;
+	}
+
+	public function get($url, $data = NULL, $headers = NULL, $cookie = NULL) {
+		return $this->request(
+			$url,
+			$data,
+			static::GET,
+			$headers,
+			$cookie
+		);
+	}
+
+	public function post() {
+
+	}
+
+	public function request($url, $data = NULL, $method, $headers = NULL, $cookie = NULL) {
+		$this->_request = curl_init();
+
+		$options = array(
+			CURLOPT_URL => $url,
+			CURLOPT_CUSTOMREQUEST => $method,
+			CURLOPT_HEADER => TRUE,
+			CURLOPT_NOBODY => $method === static::HEAD,
+			CURLOPT_RETURNTRANSFER => TRUE,
+			CURLOPT_USERAGENT => $this->_userAgent,
+			CURLOPT_SSL_VERIFYPEER => FALSE
+		);
+
+		curl_setopt_array($this->_request, $options);
+		$result = curl_exec($this->_request);
+		$this->parseResponse($result);
+		curl_close($this->_request);
+	}
+
+	public function parseResponse($result) {
+		list($responseParts['headers'], $responseParts['body'])
+			= explode("\r\n\r\n", $result, 2);
+		// var_dump($responseParts['headers']);
+		echo "<pre>";
+		foreach (explode("\r\n", $responseParts['headers']) as $r) {
+			var_dump(explode(" ", $r));
+		}
+		
+		// echo "<pre>";
+		// var_dump(explode(' ', $t));
+		// var_dump(explode("\r\n", $responseParts['headers']));
+		echo "</pre>";
+
+
+		// var_dump($t);
+		// var_dump(explode("\r\n", $t[0]));
 	}
 }
