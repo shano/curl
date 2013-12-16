935085c36f5e5f9c4af08eb1a6a03fc5a909ae59
Alexey Plekhanov <oleksii.plekhanov@gmail.com>
2013-12-16 23:44:02 +0200

    Added post method, added data, headers and cookies for queries

--- src/Alexsoft/Curl.php
+++ src/Alexsoft/Curl.php
@@ -32,7 +32,7 @@ class Curl {
 	protected $_userAgent;
 
 	public function __construct() {
-		$this->setUserAgent('alexsoft/laravel-curl');
+		$this->setUserAgent('alexsoft/curl v' . static::VERSION);
 	}
 
 	public function setUserAgent($userAgent) {
@@ -40,43 +40,83 @@ class Curl {
 		return $this;
 	}
 
-	public function get($url, $data = NULL, $headers = NULL, $cookie = NULL) {
+	public function get($url, $data = NULL, $headers = NULL, $cookies = NULL) {
 		return $this->request(
 			$url,
 			$data,
 			static::GET,
 			$headers,
-			$cookie
+			$cookies
 		);
 	}
 
-	public function head($url, $data = NULL, $headers = NULL, $cookie = NULL) {
+	public function head($url, $data = NULL, $headers = NULL, $cookies = NULL) {
 		return $this->request(
 			$url,
 			$data,
 			static::HEAD,
 			$headers,
-			$cookie
+			$cookies
 		);
 	}
 
-	public function request($url, $data = NULL, $method, $headers = NULL, $cookie = NULL) {
+	public function post($url, $data = NULL, $headers = NULL, $cookies = NULL) {
+		return $this->request(
+			$url,
+			$data,
+			static::POST,
+			$headers,
+			$cookies
+		);
+	}
+
+	public function request($url, $data = NULL, $method, $headers = NULL, $cookies = NULL) {
+		// Set data for GET queries
 		if ($method === static::GET && isset($data)) {
-			$url = trim($url, '/');
+			$url = trim($url, '/') . '?';
 			$url .= is_array($data) ? http_build_query($data) : $data;
 		}
 
 		$this->_request = curl_init();
 
+		// Set options
 		$options = array(
 			CURLOPT_URL => $url,
-			CURLOPT_CUSTOMREQUEST => $method,
+			CURLOPT_POST => $method === static::POST,
 			CURLOPT_HEADER => TRUE,
 			CURLOPT_NOBODY => $method === static::HEAD,
 			CURLOPT_RETURNTRANSFER => TRUE,
 			CURLOPT_USERAGENT => $this->_userAgent,
 			CURLOPT_SSL_VERIFYPEER => FALSE
 		);
+
+		if (!in_array($method, array(static::GET, static::HEAD, static::POST))) {
+			$options[CURLOPT_CUSTOMREQUEST] = $method;
+		}
+
+		// Set data for not GET queries
+		if (isset($data) && $method !== static::GET) {
+			$options[CURLOPT_POSTFIELDS]
+				= is_array($data) ? http_build_query($data) : $data;
+		}
+
+		// Set headers if needed
+		if (isset($headers)) {
+			$headersToSend = array();
+			foreach ($headers as $key => $value) {
+				$headersToSend[] = "{$key}: {$value}";
+			}
+			$options[CURLOPT_HEADER] = $headersToSend;
+		}
+
+		// Set cookies if needed
+		if (isset($cookies)) {
+			$cookiesToSend = array();
+			foreach ($cookies as $key => $value) {
+				$cookiesToSend[] = "{$key}={$value}";
+			}
+			$options[CURLOPT_COOKIE] = implode('; ', $cookiesToSend);
+		}
 
 		curl_setopt_array($this->_request, $options);
 		$this->_response = curl_exec($this->_request);
@@ -91,6 +131,7 @@ class Curl {
 	protected function _parseResponse() {
 		if (isset($this->_response)) {
 			list($responseParts['headersString'], $responseParts['body']) = explode("\r\n\r\n", $this->_response, 2);
+			$responseParts['body'] = htmlspecialchars($responseParts['body']);
 
 			$headers = explode("\r\n", $responseParts['headersString']);
 			$cookies = array();
